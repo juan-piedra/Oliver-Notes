e393158bc8b5f8100418ca00df8ea07e9a0aa1f4
linuscth <1020cheung@gmail.com>
2023-09-10 16:43:14 -0700

    modify app.jsx, and added searchClass page

---
+++ client/src/components/UI/NavUI.jsx
@@ -0,0 +1,16 @@
+export default function Nav({ links }) {
+    return (
+        <nav className="navbar navbar-expand-lg  rounded-1 rounded-top-0 bg-opacity-10">
+            <div className="container-fluid">
+                <div className=" navbar-collapse" id="navbarSupportedContent">
+                    <div className="d-flex mb-3 justify-content-between">
+                        <h1 className="me-auto p-3 text-white-50 " id="navTitle">Olive Notes </h1>
+                        <ul className="navbar-nav  p-3 fs-5">
+                            {links.map((link) => link)}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
