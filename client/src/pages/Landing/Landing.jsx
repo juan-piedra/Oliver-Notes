3d7c2fd4ae1052465caccd4ce909a9cfc79ff9d7
Gene Suhir <gsuhir@gmail.com>
2023-09-12 20:01:50 -0700

    landing page styling adjustments and text additions

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -7,21 +7,16 @@ const Login = () => {
         <div class="image-container">
           <div class="main">
             <div class="main-container">
-              <h2>Quick and Cheap Notes!</h2>
-              <button class="main-signup-btn">Sign Up</button>
+              <h2>Oliver Notes</h2>
+              <button class="main-signup-btn">Sign Up/Login</button>
             </div>
           </div>
         </div>
       </div>
       <div class="bottom-half">
-        <h3>About</h3>
+        <h3>About Us</h3>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore
-          reiciendis vitae ex cupiditate eius facere aliquam ut quam nostrum rem
-          minima consequatur fugiat ea odio nulla libero sit voluptatum, dicta
-          velit sequi? Ab, adipisci saepe qui obcaecati iure ad voluptatum?
-          Illum provident assumenda laborum odit amet! Dolorum delectus
-          repellendus tenetur.
+          Oliver Notes is a premium crowdsourcing service for college students looking to make some extra cash by selling their notes and those who want to buy notes for courses at their university.  Inspired by much-ballyhooed film director Oliver Stone ("Notes" is an anagram), students seeking to find notes will see results based on an upvote/downvote system.  Those who wish to sell their notes will upload images and then when their notes get bought, they'll get paid on Any Given Sunday.
         </p>
       </div>
     </>
