29fc5bc259be81622317d1a265dd7397b7b5cba3
linuscth <1020cheung@gmail.com>
2023-09-18 21:11:06 -0700

    added classes

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,57 +1,122 @@
 import "./Search.css";
-import { Link, Navigate, useLocation } from "react-router-dom";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Auth from "../../utils/Auth";
-import { useQuery } from "@apollo/client";
-import { QUERY_SCHOOLS } from "../../utils/queries";
+import { useQuery, useLazyQuery } from "@apollo/client";
+import { QUERY_SCHOOLS, QUERY_CLASSES } from "../../utils/queries";
+import { useEffect, useState } from "react";
 
 const Search = () => {
   let currentLocation = useLocation();
   const { loading, data } = useQuery(QUERY_SCHOOLS);
   const schoolData = data?.schools || [];
   console.log(schoolData);
+  const [formData, setFormData] = useState({ SchoolId: '', classId: '' });
+  const [classOptions, setClassOptions] = useState([]);
+
+  let navigate = useNavigate();
+
+  // Move the useLazyQuery outside of the Hello function
+  const [loadClass, { called, loading: classLoading, data: classData }] = useLazyQuery(
+    QUERY_CLASSES,
+    { variables: { SchoolId: formData.SchoolId } }
+  );
+
+  useEffect(() => {
+    console.log("///", classData);
+
+    if (classData?.Classes) {
+      setClassOptions(classData.Classes.classes);
+    }
+  }, [classData]);
+
+  useEffect(() => {
+    console.log(formData.SchoolId);
+    loadClass({ variables: { schoolId: formData.SchoolId } });
+  }, [formData.SchoolId]);
+
+  const handleInputChange = async (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+
+    if (name === "SchoolId") {
+      // Removed the Hello function, and instead, call loadClass directly here
+      console.log(value);
+    }
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    if (formData.classId) {
+      navigate(`/Notes/${formData.classId}`);
+    } else {
+      console.error("Please select a class.");
+    }
+    setFormData({
+      SchoolId: '',
+      classId: ''
+    });
+  };
 
   return (
     <>
       {Auth.loggedIn() ? (
         <>
           {(window.currentLocation = "/Search" ? <Navbar /> : null)}
-          <main className="search-main">
-            <section className="search-left-side">
-              <div className="search-container">
-                <select
-                  id="schoolVal"
-                  className="search form-select"
-                  aria-label="Default select example"
-                >
-                  <option selected>Select school</option>
-                  {schoolData.map((school) => {
-                    return (
-                      <option value={school._id}>{school.schoolName}</option>
-                    );
-                  })}
-                </select>
-              </div>
-              <div className="search-container">
-                <select
-                  id="classVal"
-                  className="search form-select"
-                  aria-label="Default select example"
-                >
-                  <option selected>Select Name</option>
-                  <option value="1">Class 1</option>
-                  <option value="2">Class 2</option>
-                  <option value="3">Class 3</option>
-                </select>
-              </div>
-              <Link to="/AddClass">
-                <button>Can't find your class?</button>
-              </Link>
-            </section>
-            <section className="search-right-side">
-              <button className="search-btn">Show Me The Notes!</button>
-            </section>
-          </main>{" "}
+          <main>
+            <form className="search-main" onSubmit={handleFormSubmit}>
+              <section className="search-left-side">
+                <div className="search-container">
+                  <select
+                    id="schoolVal"
+                    className="search form-select"
+                    aria-label="Default select example"
+                    onChange={handleInputChange}
+                    name="SchoolId"
+                    value={formData.SchoolId}
+                  >
+                    <option value="" selected>
+                      Select school
+                    </option>
+                    {schoolData.map((school) => {
+                      return (
+                        <option value={school._id} key={school._id}>
+                          {school.schoolName}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <div className="search-container">
+                  <select
+                    id="classVal"
+                    className="search form-select"
+                    aria-label="Default select example"
+                    onChange={handleInputChange}
+                    name="classId"
+                    value={formData.classId}
+                  >
+                    <option value="" selected>
+                      Select Class Name
+                    </option>
+                    {classOptions.map((classItem) => (
+                      <option value={classItem._id} key={classItem._id}>
+                        {classItem.className}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <Link to="/AddClass">
+                  <button>Can't find your class?</button>
+                </Link>
+              </section>
+              <section className="search-right-side">
+                <button className="search-btn" type="submit">
+                  Show Me The Notes!
+                </button>
+              </section>
+            </form>
+          </main>
         </>
       ) : (
         <Navigate replace to="/Login" />
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -17,7 +17,7 @@ const SignUp = () => {
     setFormData({ ...formData, [name]: value });
   };
   const handleFormSubmit = async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     try {
       const { data } = await addUser({
         variables: { ...formData },
@@ -34,33 +34,33 @@ const SignUp = () => {
       password: '',
     });
   }
-  
+
   return (
-      <div className="signup-background-container">
-        <div className="signup-container">
-          <h2 className="signup-title">Sign Up</h2>
-          <form onSubmit={handleFormSubmit}>
-            <div className="signup-input-group">
-              <label>Username:</label>
-              <input type="text" name="username" onChange={handleInputChange} value={formData.username} />
-            </div>
-            <div className="signup-input-group">
-              <label>Email:</label>
-              <input type="email" name="email" onChange={handleInputChange} value={formData.email} />
-            </div>
-            <div className="signup-input-group">
-              <label>Password:</label>
-              <input type="password" name="password" onChange={handleInputChange} value={formData.password} />
-            </div>
-            <button type="submit" className="signup-btn">
-              Sign Up
-            </button>
-          </form>
-          <Link to='/Login'>
-            <p>Already have an account?</p>
-          </Link>
-        </div>
+    <div className="signup-background-container">
+      <div className="signup-container">
+        <h2 className="signup-title">Sign Up</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="signup-input-group">
+            <label>Username:</label>
+            <input type="text" name="username" onChange={handleInputChange} value={formData.username} />
+          </div>
+          <div className="signup-input-group">
+            <label>Email:</label>
+            <input type="email" name="email" onChange={handleInputChange} value={formData.email} />
+          </div>
+          <div className="signup-input-group">
+            <label>Password:</label>
+            <input type="password" name="password" onChange={handleInputChange} value={formData.password} />
+          </div>
+          <button type="submit" className="signup-btn">
+            Sign Up
+          </button>
+        </form>
+        <Link to='/Login'>
+          <p>Already have an account?</p>
+        </Link>
       </div>
+    </div>
   );
 };
 
