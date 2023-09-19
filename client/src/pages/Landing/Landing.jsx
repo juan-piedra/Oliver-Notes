8a38d369eb31d1286b856c1e34dd71e3e178f6ac
Gene Suhir <gsuhir@gmail.com>
2023-09-18 20:20:35 -0700

    fixed typo

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -18,7 +18,7 @@ const Landing = () => {
       <div class="landing-bottom-half">
         <h3>About Us</h3>
         <p>
-          Oliver Notes is a premium crowdsourcing service for college students looking to make some extra cash by selling their notes and those who want to buy notes for courses at their university.  Inspired by much-ballyhooed film director Oliver Stone ("Notes" is an anagram), students seeking to find notes will see results based listed down...and to the left.  Those who wish to sell their notes will upload images and then when their notes get bought, they'll get paid on Any Given Sunday.
+          Oliver Notes is a premium crowdsourcing service for college students looking to make some extra cash by selling their notes and those who want to buy notes for courses at their university.  Inspired by much-ballyhooed film director Oliver Stone ("Notes" is an anagram), students seeking to find notes will see results listed when looking down...and to the left.  Those who wish to sell their notes will upload images and then when their notes get bought, they'll get paid on Any Given Sunday.
         </p>
       </div>
     </main>
