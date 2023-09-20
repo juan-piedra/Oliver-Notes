792b818f2612de9a91c824ec8cfe5169f3bd5a7f
Juan Piedra <juan.piedra@rocketmail.com>
2023-09-19 18:31:56 -0700

    final touches

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -14,26 +14,9 @@ db.once('open', async () => {
         const schools = await School.find({});
         const users = await User.find({});
         const randomUserId = () => users[Math.floor(Math.random() * users.length)]._id;
-        const newNoteArr = [
-            {
-                price: "$5.99",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                pdf: 'https://docs.google.com/document/d/1-2EE1JhIWOsKaIjaM5DCcxp3sZqW_k6gMtTkJO3azzM/edit',
-
-            },
-            {
-                price: "$7.99",
-                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                pdf: 'https://docs.google.com/document/d/1-2EE1JhIWOsKaIjaM5DCcxp3sZqW_k6gMtTkJO3azzM/edit',
-
-            }]
         const newClassesArr = [
             {
                 className: "History 102",
-                notes: []
-            },
-            {
-                className: " Communications 101",
                 notes: []
             },
             {
@@ -56,6 +39,7 @@ db.once('open', async () => {
                 })))
                 singleClass.notes = createClass.map(note => note._id)
                 await school.save();
+                console.log("Processing...")
             }
         }
 
